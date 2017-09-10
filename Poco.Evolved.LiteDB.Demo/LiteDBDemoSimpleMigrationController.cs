e48d6cb82e13bc008df66433bb0a363bd65f154d
Philipp Spiegel <spiegelp@gmx.at>
2017-09-10 15:38:45 +0200

    demo for Poco.Evolved.LiteDB

---
+++ Poco.Evolved.LiteDB.Demo/LiteDBDemoSimpleMigrationController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Poco.Evolved.Core.Database;
+using Poco.Evolved.Core.Transactions;
+using Poco.Evolved.LiteDB;
+using Poco.Evolved.LiteDB.Transactions;
+
+namespace Poco.Evolved.LiteDB.Demo
+{
+    public class LiteDBDemoSimpleMigrationController : LiteDBSimpleMigrationController
+    {
+        public override long CurrentVersionNumber
+        {
+            get
+            {
+                return 3;
+            }
+        }
+
+        public LiteDBDemoSimpleMigrationController(IUnitOfWorkFactory<LiteDBUnitOfWork> unitOfWorkFactory, IDatabaseHelper<LiteDBUnitOfWork> databaseHelper)
+            : base(unitOfWorkFactory, databaseHelper) { }
+
+        protected override void ApplyMigration(LiteDBUnitOfWork unitOfWork, long versionNumber, out string description, out string checksum)
+        {
+            description = null;
+            checksum = null;
+
+            List<Person> persons = unitOfWork.LiteRepository.Fetch<Person>();
+
+            switch (versionNumber)
+            {
+                case 1:
+                    // extend the names
+                    persons.ForEach(person =>
+                    {
+                        person.Name = person.Name + " " + person.Name.ToUpper();
+                    });
+
+                    unitOfWork.LiteRepository.Update<Person>(persons);
+
+                    description = "extend name";
+
+                    break;
+
+                case 2:
+                    // add age to the persons
+                    persons.ForEach(person =>
+                    {
+                        person.Age = 20;
+                    });
+
+                    unitOfWork.LiteRepository.Update<Person>(persons);
+
+                    description = "add age";
+
+                    break;
+
+                case 3:
+                    // change the age of the persons
+                    persons.ForEach(person =>
+                    {
+                        person.Age = person.Age + 2;
+                    });
+
+                    unitOfWork.LiteRepository.Update<Person>(persons);
+
+                    description = "change age";
+
+                    break;
+            }
+        }
+    }
+}
---
+++ Poco.Evolved.LiteDB.Demo/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+using LiteDB;
+
+using Poco.Evolved.LiteDB;
+using Poco.Evolved.LiteDB.Database;
+using Poco.Evolved.LiteDB.Transactions;
+
+namespace Poco.Evolved.LiteDB.Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                // simple migrations
+                using (LiteRepository liteRepository = InitDatabase())
+                {
+                    Console.WriteLine("before migrations:");
+                    PrintPersons(liteRepository);
+
+                    LiteDBSimpleMigrationController controller = new LiteDBDemoSimpleMigrationController(
+                        new LiteDBUnitOfWorkFactory(liteRepository),
+                        new LiteDBDatabaseHelper()
+                    );
+                    controller.ApplyMigrations();
+
+                    Console.WriteLine("\nafter migrations:");
+                    PrintPersons(liteRepository);
+                }
+
+                Console.WriteLine("\nPress enter to exit...");
+                Console.Read();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc);
+            }
+        }
+
+        private static LiteRepository InitDatabase()
+        {
+            string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            int index = assemblyLocation.LastIndexOf(@"\");
+            string assemblyDirectory = assemblyLocation.Substring(0, index);
+            string databaseFilename = assemblyDirectory + @"\database.litedb";
+
+            // delete old test database
+            if (File.Exists(databaseFilename))
+            {
+                File.Delete(databaseFilename);
+            }
+
+            // init mapping
+            BsonMapper.Global.Entity<Person>()
+                .Id(e => e.Id)
+                .Field(e => e.Name, nameof(Person.Name))
+                .Field(e => e.Age, nameof(Person.Age));
+
+            // create new test database
+            LiteRepository liteRepository = new LiteRepository("Filename=" + databaseFilename);
+
+            List<Person> persons = new List<Person>
+            {
+                new Person() { Name = "Alice" },
+                new Person() { Name = "Bob" },
+                new Person() { Name = "Trudy" }
+            };
+
+            liteRepository.Insert<Person>(persons);
+
+            return liteRepository;
+        }
+
+        private static void PrintPersons(LiteRepository liteRepository)
+        {
+            IEnumerable<Person> persons = liteRepository.Fetch<Person>()
+                .OrderBy(person => person.Name);
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine("{0}, {1} years old", person.Name, person.Age);
+            }
+        }
+    }
+}
